8cf4e104eb6d7e5ba8c82695be46693430179e97
Vladyslav <knyshv2412@gmail.com>
2017-06-18 03:08:44 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or "postgresql://knyshv:38kv1444@localhost/names_evo_summer_test_db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
 from app import views, models
